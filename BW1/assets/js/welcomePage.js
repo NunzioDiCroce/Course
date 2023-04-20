2450803c37c451709933060e3d473a0b2043ceb1
deve loper <deve25loper@gmail.com>
2023-04-20 15:31:47 +0200

    commit

--- BW1/assets/js/welcomePage.js
+++ BW1/assets/js/welcomePage.js
@@ -3,17 +3,17 @@
 
 var check = document.getElementById('customCheckbox');
 var btnPage1 = document.getElementById('buttonWelcomePage');
-check.checked = false; /*da verificare*/
-btnPage1.disabled = true; /*da verificare*/
+check.checked = false;
+btnPage1.disabled = true;
 
-    check.addEventListener('click', function(){
-        if(this.checked){
-            btnPage1.className = 'button';
-            btnPage1.classList.add('customActive');
-            btnPage1.disabled = false;
-        } else{ /*da verificare*/
-            btnPage1.disabled = true;   
-            btnPage1.classList.remove('customActive');
-            btnPage1.classList.remove('button');
+check.addEventListener('click', function(){
+    if(this.checked){
+        btnPage1.className = 'button';
+        btnPage1.classList.add('customActive');
+        btnPage1.disabled = false;
+    } else{
+        btnPage1.disabled = true;   
+        btnPage1.classList.remove('customActive');
+        btnPage1.classList.remove('button');
         }
-    });
+});
