10f256c46335b6a3c8c663eaf6809edb5af93e61
deve loper <deve25loper@gmail.com>
2023-05-01 15:00:38 +0200

    commit

--- ProgettoSettimana05/assets/js/script.js
+++ ProgettoSettimana05/assets/js/script.js
@@ -1,12 +1,26 @@
 /* - - - - - - - - - - navbar color change - - - - - - - - - - */
-var headerClassName = 'headerChange';
-var getStartedClassName = 'getStartedChange';
-var scrollTrigger = 300;
+var headerChange = 'headerChange';
+var headerBack = 'headerBack';
+var getStartedChange = 'getStartedChange';
+var getStartedBack = 'getStartedBack';
+var scrollTrigger = 240;
 
 window.onscroll = function () {
-    if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger ) {
-        document.getElementsByTagName('header')[0].classList.add(headerClassName)
+    if (window.scrollY >= scrollTrigger) {
+        document.getElementsByTagName('header')[0].classList.add('headerChange');
+        document.getElementsByTagName('header')[0].classList.remove('headerBack');
+        document.getElementsByClassName('getStarted')[0].classList.add('getStartedChange');
+        document.getElementsByClassName('getStarted')[0].classList.remove('getStartedBack');
     } else {
-        document.getElementsByTagName('header')[0].classList.remove(headerClassName);
+        document.getElementsByTagName('header')[0].classList.remove('headerChange');
+        document.getElementsByTagName('header')[0].classList.add('headerBack');
+        document.getElementsByClassName('getStarted')[0].classList.remove('getStartedChange');
+        document.getElementsByClassName('getStarted')[0].classList.add('getStartedBack');
     }
 }
+
+console.log(window.scrollY);
+
+var button = document.getElementById('getStarted');
+console.log (button.classList);
+
