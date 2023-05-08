f7e2f61a16efa2d16d921bf3fe151b3042cf2b21
deve loper <deve25loper@gmail.com>
2023-05-08 16:03:49 +0200

    commit

--- week07_day01/assets/js/script.js
+++ week07_day01/assets/js/script.js
@@ -6,6 +6,27 @@ class User {
         this.age = _age;
         this.location = _location
     }
-    
+    benchmark () {
+        if (firstUser.age > secondUser.age) {
+            console.log(`${firstUser.firstName} ${firstUser.lastName} è più vecchio di ${secondUser.firstName} ${secondUser.lastName}`)
+        } else if (firstUser.age === secondUser.age) {
+            console.log(`${firstUser.firstName} ${firstUser.lastName} e ${secondUser.firstName} ${secondUser.lastName} hanno la stessa età`)
+        } else {
+            console.log(`${firstUser.firstName} ${firstUser.lastName} è più giovane di ${secondUser.firstName} ${secondUser.lastName}`)
+        }
+    }
 }
+
+var firstUser = new User ('Mario', 'Rossi', 105, 'Roma');
+var secondUser = new User ('Paolo', 'Bianchi', 120, 'Milano');
+firstUser.benchmark();
+
+console.log(firstUser);
+console.log(secondUser);
+
+
+
+
+
+
 
