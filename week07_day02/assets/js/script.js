69ac6cb2eb73685f129dd262c21f13b2760622ff
deve loper <deve25loper@gmail.com>
2023-05-09 15:18:19 +0200

    commit

--- week07_day02/assets/js/script.js
+++ week07_day02/assets/js/script.js
@@ -1,3 +1,9 @@
+
 //definizione variabili DOM
+//definizione variabili DOM: buttons
 let addUser = document.getElementById('addBtn');
 let removeUser = document.getElementById('removeBtn');
+//definizione variabili DOM: form fields
+let userName = document.getElementById('nameBox');
+let userSurname = document.getElementById('surnameBox');
+let userCity = document.getElementById('cityBox');
