62bc7eaf347b83014a045e92d55419248677ec13
deve loper <deve25loper@gmail.com>
2023-05-09 15:52:01 +0200

    commit

--- week07_day02/assets/js/script.js
+++ week07_day02/assets/js/script.js
@@ -3,17 +3,29 @@
 //definizione variabili DOM: buttons
 let addUser = document.getElementById('addBtn');
 let removeUser = document.getElementById('removeBtn');
+
 //definizione variabili DOM: form fields
 let userName = document.getElementById('nameBox');
 let userSurname = document.getElementById('surnameBox');
 let userCity = document.getElementById('cityBox');
 
+//definizione array users
+let users = [];
+
 //funzione "Aggiungi utente"
 addUser.addEventListener('click', function(event) {
-
+    event.preventDefault();
+    let newUser = {
+        name: userName.value,
+        surname: userSurname.value,
+        city: userCity.value
+    }
+    console.log(newUser);
+    users.push(newUser);
+    localStorage.setItem('newUser', JSON.stringify(users));
 });
 
 //funzione "Rimuovi utente"
 removeUser.addEventListener('click', function(event) {
-    
+
 });
