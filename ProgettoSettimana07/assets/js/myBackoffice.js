3698997723c0a6f00b05c90cbc1b06178f0c3862
NunzioDiCroce <nunziodavide@gmail.com>
2023-05-12 14:29:18 +0200

    commit

--- ProgettoSettimana07/assets/js/myBackoffice.js
+++ ProgettoSettimana07/assets/js/myBackoffice.js
@@ -1,6 +1,6 @@
 
 // - - - - - - - - - - - - - - - FETCH
-const myFunction = async () => {
+/*const myFunction = async () => {
     const promise = await fetch('https://striveschool-api.herokuapp.com/api/product/', {
         headers: {
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDVlMTA2ZTg4Zjc0MDAwMTQyODc1NDMiLCJpYXQiOjE2ODM4ODYxOTAsImV4cCI6MTY4NTA5NTc5MH0.SONJVTCh6D-4h1tQmGvDBURkzhteVbnm5cQqR7lb0kw"
@@ -13,4 +13,37 @@ const myFunction = async () => {
         throw new Error("Richiesta non a buon fine")
     }
 
+}*/
+
+const submitFunction = async (event) => {
+    event.preventDefault();
+
+    const newProduct = {
+        name: document.getElementById('name').value,
+        description: document.getElementById('description').value,
+        brand: document.getElementById('brand').value,
+        imageUrl: document.getElementById('imageUrl').value,
+        price: document.getElementById('price').value
+    }
+    console.log('nuovo prodotto inserito', newProduct)
+
+    try {
+        const promise = await fetch('https://striveschool-api.herokuapp.com/api/product/', {
+            method: "POST",
+            body: JSON.stringify(newProduct),
+            headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDVlMTA2ZTg4Zjc0MDAwMTQyODc1NDMiLCJpYXQiOjE2ODM4ODYxOTAsImV4cCI6MTY4NTA5NTc5MH0.SONJVTCh6D-4h1tQmGvDBURkzhteVbnm5cQqR7lb0kw",
+                "Content-Type": "application/json"
+                }
+            })
+            if (promise.ok) {
+                const newProductPosted = await promise.json();
+                console.log(newProductPosted)
+            } else {
+                throw new Error("Richiesta non a buon fine")
+            }
+
+    } catch (error) {
+        alert(error)
+    } 
 }
