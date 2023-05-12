f00788baa33d829a7eb02e9d89fc2de03d071fcc
NunzioDiCroce <nunziodavide@gmail.com>
2023-05-12 13:39:46 +0200

    commit

---
+++ ProgettoSettimana07/assets/js/myBackoffice.js
@@ -0,0 +1,16 @@
+
+// - - - - - - - - - - - - - - - FETCH
+const myFunction = async () => {
+    const promise = await fetch('https://striveschool-api.herokuapp.com/api/product/', {
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDVlMTA2ZTg4Zjc0MDAwMTQyODc1NDMiLCJpYXQiOjE2ODM4ODYxOTAsImV4cCI6MTY4NTA5NTc5MH0.SONJVTCh6D-4h1tQmGvDBURkzhteVbnm5cQqR7lb0kw"
+        }
+    })
+    if (promise.ok) {
+        const response = await promise.json()
+        console.log(response)
+    } else {
+        throw new Error("Richiesta non a buon fine")
+    }
+
+}
