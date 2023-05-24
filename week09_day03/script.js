5df86d0d24dff385eb032f1beb7221656d27aff8
NunzioDiCroce <nunziodavide@gmail.com>
2023-05-24 16:35:33 +0200

    commit

--- week09_day03/script.js
+++ week09_day03/script.js
@@ -1,5 +1,20 @@
-// - - - - - - - - - - - - - - - SOLUZIONE 1 - - - - - - - - - -
-console.log('- - - - - - - - - - SOLUZIONE 1 - - - - - - - - - -');
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// - - - - - - - - - - - - - - - SOLUZIONE CON UNA CLASSE
+console.log('- - - - - - - - - - SOLUZIONE CON UNA CLASSE');
 // definizione classe Lavoratore
 var Lavoratore = /** @class */ (function () {
     function Lavoratore(_codRedd, _redditoAnnuoLordo, _tasseInps, _tasseIrpef) {
@@ -35,5 +50,43 @@ console.log('Imponibile annuo: ' + lavoratore.getUtileTasse(codRedd_input, reddi
 console.log('Inps: ' + lavoratore.tasseInps + '%' + ' ovvero ' + lavoratore.getTasseInps(tasseInps_input) + ' €');
 console.log('Irpef: ' + lavoratore.tasseIrpef + '%' + ' ovvero ' + lavoratore.getTasseIrpef(tasseIrpef_input) + ' €');
 console.log('Reddito annuo netto: ' + lavoratore.getRedditoAnnuoNetto() + ' €');
-// - - - - - - - - - - - - - - - SOLUZIONE 2 - - - - - - - - - -
-console.log('- - - - - - - - - - SOLUZIONE 2 - - - - - - - - - -');
+// - - - - - - - - - - - - - - - SOLUZIONE CON CLASSE ABSTRACT
+console.log('- - - - - - - - - - SOLUZIONE CON CLASSE ABSTRACT');
+// definizione classe astratta Dipendente
+var Contribuente = /** @class */ (function () {
+    function Contribuente(_codice, _reddito, _inps, _irpef) {
+        this.codice = _codice;
+        this.reddito = _reddito;
+        this.inps = _inps;
+        this.irpef = _irpef;
+    }
+    return Contribuente;
+}());
+// definizione classe Dipendente come estensione di Contribuente
+var Dipendente = /** @class */ (function (_super) {
+    __extends(Dipendente, _super);
+    function Dipendente(_codice, _reddito, _inps, _irpef) {
+        return _super.call(this, _codice, _reddito, _inps, _irpef) || this;
+    }
+    Dipendente.prototype.getImponibile = function () {
+        return (this.codice / 100) * this.reddito;
+    };
+    Dipendente.prototype.getInps = function () {
+        return (this.inps / 100) * this.getImponibile();
+    };
+    Dipendente.prototype.getIrpef = function () {
+        return (this.irpef / 100) * this.getImponibile();
+    };
+    Dipendente.prototype.getUtile = function () {
+        return this.reddito - this.getInps() - this.getIrpef();
+    };
+    return Dipendente;
+}(Contribuente));
+// istanza classe Lavoratore + console.log
+var dipendente = new Dipendente(70, 200000, 10, 10);
+console.log('Codice redditività: ' + dipendente.codice + '%');
+console.log('Reddito annuo lordo: ' + dipendente.reddito + ' €');
+console.log('Imponibile annuo: ' + dipendente.getImponibile() + ' €');
+console.log('Inps: ' + dipendente.inps + '%' + ' ovvero ' + dipendente.getInps() + ' €');
+console.log('Irpef: ' + dipendente.irpef + '%' + ' ovvero ' + dipendente.getIrpef() + ' €');
+console.log('Reddito annuo netto: ' + dipendente.getUtile() + ' €');
--- week09_day03/script.ts
+++ week09_day03/script.ts
@@ -1,5 +1,5 @@
-// - - - - - - - - - - - - - - - SOLUZIONE 1 - - - - - - - - - -
-console.log('- - - - - - - - - - SOLUZIONE 1 - - - - - - - - - -')
+// - - - - - - - - - - - - - - - SOLUZIONE CON UNA CLASSE
+console.log('- - - - - - - - - - SOLUZIONE CON UNA CLASSE')
 // definizione classe Lavoratore
 class Lavoratore {
     codRedd:number;
@@ -43,5 +43,50 @@ console.log('Reddito annuo netto: ' + lavoratore.getRedditoAnnuoNetto() + ' €'
 
 
 
-// - - - - - - - - - - - - - - - SOLUZIONE 2 - - - - - - - - - -
-console.log('- - - - - - - - - - SOLUZIONE 2 - - - - - - - - - -')
+// - - - - - - - - - - - - - - - SOLUZIONE CON CLASSE ABSTRACT
+console.log('- - - - - - - - - - SOLUZIONE CON CLASSE ABSTRACT')
+// definizione classe astratta Dipendente
+abstract class Contribuente {
+    codice:number;
+    reddito:number;
+    inps:number;
+    irpef:number;
+    constructor(_codice:number, _reddito:number, _inps:number, _irpef:number) {
+        this.codice = _codice;
+        this.reddito = _reddito;
+        this.inps = _inps;
+        this.irpef = _irpef
+    }
+    abstract getImponibile();
+    abstract getInps();
+    abstract getIrpef();
+    abstract getUtile();
+}
+
+// definizione classe Dipendente come estensione di Contribuente
+class Dipendente extends Contribuente {
+    constructor(_codice:number, _reddito:number, _inps:number, _irpef:number) {
+        super(_codice, _reddito, _inps, _irpef)
+    }
+    getImponibile() {
+        return (this.codice/100) * this.reddito
+    }
+    getInps() {
+        return (this.inps/100) * this.getImponibile()
+    }
+    getIrpef() {
+        return (this.irpef/100) * this.getImponibile()
+    }
+    getUtile() {
+        return this.reddito - this.getInps() - this.getIrpef()
+    }
+}
+
+// istanza classe Lavoratore + console.log
+let dipendente = new Dipendente (70, 200000, 10, 10);
+console.log('Codice redditività: ' + dipendente.codice + '%');
+console.log('Reddito annuo lordo: ' + dipendente.reddito + ' €');
+console.log('Imponibile annuo: ' + dipendente.getImponibile() + ' €');
+console.log('Inps: ' + dipendente.inps +'%' + ' ovvero ' + dipendente.getInps() + ' €');
+console.log('Irpef: ' + dipendente.irpef +'%' + ' ovvero ' + dipendente.getIrpef() + ' €');
+console.log('Reddito annuo netto: ' + dipendente.getUtile() + ' €');
