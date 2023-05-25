1efbe24f78569315b44550dcf339ced55778e926
NunzioDiCroce <nunziodavide@gmail.com>
2023-05-25 16:10:32 +0200

    commit

--- week09_day04/script.js
+++ week09_day04/script.js
@@ -1,3 +1,4 @@
+// definizione classe "Capo"
 var Capo = /** @class */ (function () {
     function Capo(_id, _codprod, _collezione, _capo, _modello, _quantita, _colore, _prezzoIvaEsclusa, _prezzoIvaInclusa, _disponibile, _saldo) {
         this.id = _id;
@@ -20,23 +21,37 @@ var Capo = /** @class */ (function () {
     };
     return Capo;
 }());
+// istanze (oggetti) di classe "Capo"
+// istanza 1
 var capoA = new Capo(1, 2121, 'primavera', 'cardigan', 1231, 5, 'nero', 18.50, 22.57, 'negozio', 45);
 console.log(capoA);
 console.log('Saldo capo = ' + capoA.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoA.getAcquistoCapo() + '€');
+// istanza 2
 var capoB = new Capo(2, 4111, 'estate', 't-shirt', 1231, 6, 'rosso', 5.50, 6.71, 'magazzino', 30);
 console.log(capoB);
 console.log('Saldo capo = ' + capoB.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoB.getAcquistoCapo() + '€');
+// istanza 3
 var capoC = new Capo(3, 1181, 'inverno', 'felpa', 1229, 8, 'beige', 17.50, 21.35, 'negozio', 50);
 console.log(capoC);
 console.log('Saldo capo = ' + capoC.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoC.getAcquistoCapo() + '€');
+// istanza 4
 var capoD = new Capo(4, 1111, 'estate', 'maglione', 1221, 4, 'verde', 20, 24.40, 'negozio', 50);
 console.log(capoD);
 console.log('Saldo capo = ' + capoD.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoD.getAcquistoCapo() + '€');
+// istanza 5
 var capoE = new Capo(5, 6111, 'primavera', 'maglia', 1021, 5, 'blu', 11, 13.42, 'magazzino', 60);
 console.log(capoE);
 console.log('Saldo capo = ' + capoE.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoE.getAcquistoCapo() + '€');
+/*window.onload = async () => {
+    try {
+        let promise = await fetch('Abbigliamento.json')
+    }
+    catch(error) {
+        alert(error)
+    }
+}*/
--- week09_day04/script.ts
+++ week09_day04/script.ts
@@ -1,4 +1,4 @@
-
+// definizione classe "Capo"
 class Capo {
     id:number;
     codprod:number;
@@ -11,7 +11,6 @@ class Capo {
     prezzoIvaInclusa:number;
     disponibile:string;
     saldo:number;
-
     constructor(_id:number, _codprod:number, _collezione:string, _capo:string, _modello:number, _quantita:number, _colore:string, _prezzoIvaEsclusa:number, _prezzoIvaInclusa:number, _disponibile:string, _saldo:number) {
         this.id = _id;
         this.codprod = _codprod;
@@ -33,27 +32,38 @@ class Capo {
     }
 }
 
+// istanze (oggetti) di classe "Capo"
+// istanza 1
 let capoA = new Capo (1, 2121, 'primavera', 'cardigan', 1231, 5, 'nero', 18.50, 22.57, 'negozio', 45)
 console.log(capoA);
 console.log('Saldo capo = ' + capoA.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoA.getAcquistoCapo() + '€');
-
+// istanza 2
 let capoB = new Capo (2, 4111, 'estate', 't-shirt', 1231, 6, 'rosso', 5.50, 6.71, 'magazzino', 30)
 console.log(capoB);
 console.log('Saldo capo = ' + capoB.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoB.getAcquistoCapo() + '€');
-
+// istanza 3
 let capoC = new Capo (3, 1181, 'inverno', 'felpa', 1229, 8, 'beige', 17.50, 21.35, 'negozio', 50)
 console.log(capoC);
 console.log('Saldo capo = ' + capoC.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoC.getAcquistoCapo() + '€');
-
+// istanza 4
 let capoD = new Capo (4, 1111, 'estate', 'maglione', 1221, 4, 'verde', 20, 24.40, 'negozio', 50)
 console.log(capoD);
 console.log('Saldo capo = ' + capoD.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoD.getAcquistoCapo() + '€');
-
+// istanza 5
 let capoE = new Capo (5, 6111, 'primavera', 'maglia', 1021, 5, 'blu', 11, 13.42, 'magazzino', 60)
 console.log(capoE);
 console.log('Saldo capo = ' + capoE.getSaldoCapo() + '€');
 console.log('Acquisto capo = ' + capoE.getAcquistoCapo() + '€');
+
+/*window.onload = async () => {
+    try {
+        let promise = await fetch('Abbigliamento.json')
+    }
+    catch(error) {
+        alert(error)
+    }
+}*/
