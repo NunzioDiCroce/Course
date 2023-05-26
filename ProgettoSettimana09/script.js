290fe44bd3390acf2acf29afc50d71c40eb3c9bb
NunzioDiCroce <nunziodavide@gmail.com>
2023-05-26 16:39:43 +0200

    commit

--- ProgettoSettimana09/script.js
+++ ProgettoSettimana09/script.js
@@ -1,3 +1,58 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+// - - - - - - - - - - - - - - - - - - - - SOLUZIONE CON INTERFACCIA + 1 CLASSE CON 3 ISTANZE - - - - - - - - - - - - - - - 
+console.log('');
+console.log('- - - - - - - - - - - - - - - SOLUZIONE CON INTERFACCIA + 1 CLASSE CON 3 ISTANZE - - - - - - - - - - - - - - - ');
 // definizione classe
 var Utente = /** @class */ (function () {
     function Utente(_credito, _numeroChiamate) {
@@ -22,20 +77,20 @@ var Utente = /** @class */ (function () {
     return Utente;
 }());
 // prima istanza + console.log
-var primoUtente = new Utente(20, 2); //credito, numeroChiamate
+var primoUtente = new Utente(30, 2); //credito, numeroChiamate
 console.log('');
-console.log('PRIMO UTENTE');
+console.log('Primo utente');
 console.log('- - - - - - - - - -');
 console.log(primoUtente);
-console.log("Ricarica: ".concat(primoUtente.ricarica(30), " \u20AC")); //importoRicarica
+console.log("Ricarica: ".concat(primoUtente.ricarica(20), " \u20AC")); //importoRicarica
 console.log("Chiamate di ".concat(primoUtente.chiamata(15), " minuti")); //minutiChiamata
-console.log("Credito residuo: ".concat(primoUtente.numero404(30, 15), " \u20AC")); //importoRicarica, minutiChiamata
+console.log("Credito residuo: ".concat(primoUtente.numero404(20, 15), " \u20AC")); //importoRicarica, minutiChiamata
 console.log("Numero chiamate: ".concat(primoUtente.getNumeroChiamate()));
 console.log("Chiamate azzerate: ".concat(primoUtente.azzeraChiamate()));
 // seconda istanza + console.log
 var secondoUtente = new Utente(0, 2); //credito, numeroChiamate
 console.log('');
-console.log('SECONDO UTENTE');
+console.log('Secondo utente');
 console.log('- - - - - - - - - -');
 console.log(secondoUtente);
 console.log("Ricarica: ".concat(secondoUtente.ricarica(20), " \u20AC")); //importoRicarica
@@ -46,7 +101,7 @@ console.log("Chiamate azzerate: ".concat(secondoUtente.azzeraChiamate()));
 // terza istanza + console.log
 var terzoUtente = new Utente(0, 4); //credito, numeroChiamate
 console.log('');
-console.log('TERZO UTENTE');
+console.log('Terzo utente');
 console.log('- - - - - - - - - -');
 console.log(terzoUtente);
 console.log("Ricarica: ".concat(terzoUtente.ricarica(10), " \u20AC")); //importoRicarica
@@ -54,3 +109,152 @@ console.log("Chiamate di ".concat(terzoUtente.chiamata(5), " minuti")); //minuti
 console.log("Credito residuo: ".concat(terzoUtente.numero404(10, 5), " \u20AC")); //importoRicarica, minutiChiamata
 console.log("Numero chiamate: ".concat(terzoUtente.getNumeroChiamate()));
 console.log("Chiamate azzerate: ".concat(terzoUtente.azzeraChiamate()));
+// - - - - - - - - - - - - - - - - - - - - SOLUZIONE CON CLASSE ASTRATTA + 1 CLASSE CON 3 ISTANZE - - - - - - - - - - - - - - - 
+console.log('');
+console.log('- - - - - - - - - - - - - - - SOLUZIONE CON CLASSE ASTRATTA + 1 CLASSE CON 3 ISTANZE - - - - - - - - - - - - - - - ');
+// definizione classe astratta
+var Abbonamento = /** @class */ (function () {
+    function Abbonamento(_credito, _numeroChiamate) {
+        this.credito = _credito;
+        this.numeroChiamate = _numeroChiamate;
+    }
+    return Abbonamento;
+}());
+// definizione classe estesa
+var nuovoAbbonamento = /** @class */ (function (_super) {
+    __extends(nuovoAbbonamento, _super);
+    function nuovoAbbonamento(_credito, _numeroChiamate, _tariffa) {
+        var _this = _super.call(this, _credito, _numeroChiamate) || this;
+        _this.tariffa = _tariffa;
+        return _this;
+    }
+    nuovoAbbonamento.prototype.ricarica = function (_importoRicarica) {
+        return _importoRicarica;
+    };
+    nuovoAbbonamento.prototype.chiamata = function (_minutiChiamata) {
+        return _minutiChiamata;
+    };
+    nuovoAbbonamento.prototype.numero404 = function (_importoRicarica, _minutiChiamata) {
+        return this.credito + this.ricarica(_importoRicarica) - (this.tariffa * this.chiamata(_minutiChiamata) * this.numeroChiamate);
+    };
+    nuovoAbbonamento.prototype.getNumeroChiamate = function () {
+        return this.numeroChiamate;
+    };
+    nuovoAbbonamento.prototype.azzeraChiamate = function () {
+        return this.numeroChiamate = 0;
+    };
+    return nuovoAbbonamento;
+}(Abbonamento));
+// prima istanza + console.log
+var primoAbbonato = new nuovoAbbonamento(30, 2, 0.2); //credito, numeroChiamate, tariffa
+console.log('');
+console.log('Primo utente');
+console.log('- - - - - - - - - -');
+console.log(primoAbbonato);
+console.log("Ricarica: ".concat(primoAbbonato.ricarica(20), " \u20AC")); //importoRicarica
+console.log("Chiamate di ".concat(primoAbbonato.chiamata(15), " minuti")); //minutiChiamata
+console.log("Credito residuo: ".concat(primoAbbonato.numero404(20, 15), " \u20AC")); //importoRicarica, minutiChiamata
+console.log("Numero chiamate: ".concat(primoAbbonato.getNumeroChiamate()));
+console.log("Chiamate azzerate: ".concat(primoAbbonato.azzeraChiamate()));
+// prima istanza + console.log
+var secondoAbbonato = new nuovoAbbonamento(30, 2, 0.4); //credito, numeroChiamate, tariffa
+console.log('');
+console.log('Secondo utente');
+console.log('- - - - - - - - - -');
+console.log(secondoAbbonato);
+console.log("Ricarica: ".concat(secondoAbbonato.ricarica(20), " \u20AC")); //importoRicarica
+console.log("Chiamate di ".concat(secondoAbbonato.chiamata(15), " minuti")); //minutiChiamata
+console.log("Credito residuo: ".concat(secondoAbbonato.numero404(20, 15), " \u20AC")); //importoRicarica, minutiChiamata
+console.log("Numero chiamate: ".concat(secondoAbbonato.getNumeroChiamate()));
+console.log("Chiamate azzerate: ".concat(secondoAbbonato.azzeraChiamate()));
+// terza istanza + console.log
+var terzoAbbonato = new nuovoAbbonamento(30, 2, 0.6); //credito, numeroChiamate, tariffa
+console.log('');
+console.log('Terzo utente');
+console.log('- - - - - - - - - -');
+console.log(terzoAbbonato);
+console.log("Ricarica: ".concat(terzoAbbonato.ricarica(20), " \u20AC")); //importoRicarica
+console.log("Chiamate di ".concat(terzoAbbonato.chiamata(15), " minuti")); //minutiChiamata
+console.log("Credito residuo: ".concat(terzoAbbonato.numero404(20, 15), " \u20AC")); //importoRicarica, minutiChiamata
+console.log("Numero chiamate: ".concat(terzoAbbonato.getNumeroChiamate()));
+console.log("Chiamate azzerate: ".concat(terzoAbbonato.azzeraChiamate()));
+// - - - - - - - - - - - - - - - - - - - - SOLUZIONE CON FETCH + SUPER CLASSE E CLASSE ESTESA - - - - - - - - - - - - - - - 
+console.log('');
+console.log('- - - - - - - - - - - - - - - SOLUZIONE CON FETCH + SUPER CLASSE E CLASSE ESTESA - - - - - - - - - - - - - - - ');
+// definizione super classe
+var Telefonia = /** @class */ (function () {
+    function Telefonia(_credito, _numeroChiamate) {
+        this.credito = _credito;
+        this.numeroChiamate = _numeroChiamate;
+    }
+    Telefonia.prototype.ricarica = function (_importoRicarica) {
+        return _importoRicarica;
+    };
+    Telefonia.prototype.chiamata = function (_minutiChiamata) {
+        return _minutiChiamata;
+    };
+    Telefonia.prototype.getNumeroChiamate = function () {
+        return this.numeroChiamate;
+    };
+    Telefonia.prototype.azzeraChiamate = function () {
+        return this.numeroChiamate = 0;
+    };
+    return Telefonia;
+}());
+// definizione classe estesa
+var telefoniaEstesa = /** @class */ (function (_super) {
+    __extends(telefoniaEstesa, _super);
+    function telefoniaEstesa(_credito, _numeroChiamate, _tariffa, _utente) {
+        var _this = _super.call(this, _credito, _numeroChiamate) || this;
+        _this.tariffa = _tariffa;
+        _this.utente = _utente;
+        return _this;
+    }
+    telefoniaEstesa.prototype.ricarica = function (_importoRicarica) {
+        return _importoRicarica;
+    };
+    telefoniaEstesa.prototype.chiamata = function (_minutiChiamata) {
+        return _minutiChiamata;
+    };
+    telefoniaEstesa.prototype.numero404 = function (_importoRicarica, _minutiChiamata) {
+        return this.credito + this.ricarica(_importoRicarica) - (this.tariffa * this.chiamata(_minutiChiamata) * this.numeroChiamate);
+    };
+    telefoniaEstesa.prototype.getNumeroChiamate = function () {
+        return this.numeroChiamate;
+    };
+    telefoniaEstesa.prototype.azzeraChiamate = function () {
+        return this.numeroChiamate = 0;
+    };
+    return telefoniaEstesa;
+}(Telefonia));
+// fetch
+window.onload = function () { return __awaiter(_this, void 0, void 0, function () {
+    var promise, utenti, i, nuovaTelefonia;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, fetch('utenti.json')];
+            case 1:
+                promise = _a.sent();
+                if (!promise.ok) return [3 /*break*/, 3];
+                return [4 /*yield*/, promise.json()];
+            case 2:
+                utenti = _a.sent();
+                console.log(utenti);
+                for (i = 0; i < utenti.length; i++) {
+                    nuovaTelefonia = new telefoniaEstesa(utenti[i].credito, utenti[i].numeroChiamate, utenti[i].tariffa, utenti[i].utente);
+                    console.log('');
+                    console.log("".concat(nuovaTelefonia.utente));
+                    console.log('- - - - - - - - - -');
+                    console.log(nuovaTelefonia);
+                    console.log("Ricarica: ".concat(nuovaTelefonia.ricarica(20), " \u20AC")); //importoRicarica
+                    console.log("Chiamate di ".concat(nuovaTelefonia.chiamata(15), " minuti")); //minutiChiamata
+                    console.log("Credito residuo: ".concat(nuovaTelefonia.numero404(20, 15), " \u20AC")); //importoRicarica, minutiChiamata
+                    console.log("Numero chiamate: ".concat(nuovaTelefonia.getNumeroChiamate()));
+                    console.log("Chiamate azzerate: ".concat(nuovaTelefonia.azzeraChiamate()));
+                }
+                return [3 /*break*/, 4];
+            case 3: throw new Error('Richiesta non a buon fine');
+            case 4: return [2 /*return*/];
+        }
+    });
+}); };
--- ProgettoSettimana09/script.ts
+++ ProgettoSettimana09/script.ts
@@ -1,4 +1,6 @@
-
+// - - - - - - - - - - - - - - - - - - - - SOLUZIONE CON INTERFACCIA + 1 CLASSE CON 3 ISTANZE - - - - - - - - - - - - - - - 
+console.log('');
+console.log('- - - - - - - - - - - - - - - SOLUZIONE CON INTERFACCIA + 1 CLASSE CON 3 ISTANZE - - - - - - - - - - - - - - - ');
 // definizione interfaccia
 interface Abbonato {
     // proprietà interfaccia
@@ -38,22 +40,22 @@ class Utente implements Abbonato {
 }
 
 // prima istanza + console.log
-let primoUtente = new Utente (20, 2); //credito, numeroChiamate
-console.log('')
-console.log('PRIMO UTENTE')
-console.log('- - - - - - - - - -')
+let primoUtente = new Utente (30, 2); //credito, numeroChiamate
+console.log('');
+console.log('Primo utente');
+console.log('- - - - - - - - - -');
 console.log(primoUtente);
-console.log(`Ricarica: ${primoUtente.ricarica(30)} €`); //importoRicarica
+console.log(`Ricarica: ${primoUtente.ricarica(20)} €`); //importoRicarica
 console.log(`Chiamate di ${primoUtente.chiamata(15)} minuti`); //minutiChiamata
-console.log(`Credito residuo: ${primoUtente.numero404(30, 15)} €`); //importoRicarica, minutiChiamata
+console.log(`Credito residuo: ${primoUtente.numero404(20, 15)} €`); //importoRicarica, minutiChiamata
 console.log(`Numero chiamate: ${primoUtente.getNumeroChiamate()}`);
 console.log(`Chiamate azzerate: ${primoUtente.azzeraChiamate()}`);
 
 // seconda istanza + console.log
 let secondoUtente = new Utente (0, 2); //credito, numeroChiamate
-console.log('')
-console.log('SECONDO UTENTE')
-console.log('- - - - - - - - - -')
+console.log('');
+console.log('Secondo utente');
+console.log('- - - - - - - - - -');
 console.log(secondoUtente);
 console.log(`Ricarica: ${secondoUtente.ricarica(20)} €`); //importoRicarica
 console.log(`Chiamate di ${secondoUtente.chiamata(10)} minuti`); //minutiChiamata
@@ -63,9 +65,9 @@ console.log(`Chiamate azzerate: ${secondoUtente.azzeraChiamate()}`);
 
 // terza istanza + console.log
 let terzoUtente = new Utente (0, 4); //credito, numeroChiamate
-console.log('')
-console.log('TERZO UTENTE')
-console.log('- - - - - - - - - -')
+console.log('');
+console.log('Terzo utente');
+console.log('- - - - - - - - - -');
 console.log(terzoUtente);
 console.log(`Ricarica: ${terzoUtente.ricarica(10)} €`); //importoRicarica
 console.log(`Chiamate di ${terzoUtente.chiamata(5)} minuti`); //minutiChiamata
@@ -75,3 +77,156 @@ console.log(`Chiamate azzerate: ${terzoUtente.azzeraChiamate()}`);
 
 
 
+// - - - - - - - - - - - - - - - - - - - - SOLUZIONE CON CLASSE ASTRATTA + 1 CLASSE CON 3 ISTANZE - - - - - - - - - - - - - - - 
+console.log('');
+console.log('- - - - - - - - - - - - - - - SOLUZIONE CON CLASSE ASTRATTA + 1 CLASSE CON 3 ISTANZE - - - - - - - - - - - - - - - ');
+// definizione classe astratta
+abstract class Abbonamento {
+        credito: number;
+        numeroChiamate: number;
+        constructor (_credito:number, _numeroChiamate:number) {
+            this.credito = _credito;
+            this.numeroChiamate = _numeroChiamate;
+        }
+        abstract ricarica(_importoRicarica:any): any;
+        abstract chiamata(_minutiChiamata:any): any;
+        abstract numero404(_importoRicarica:any, _minutiChiamata:any): any;
+        abstract getNumeroChiamate(): any;
+        abstract azzeraChiamate(): any;
+}
+
+// definizione classe estesa
+class nuovoAbbonamento extends Abbonamento {
+    tariffa: number;
+    constructor (_credito:number, _numeroChiamate:number, _tariffa:number) {
+        super (_credito, _numeroChiamate);
+        this.tariffa = _tariffa;
+    }
+    ricarica(_importoRicarica:number): number {
+        return _importoRicarica
+    }
+    chiamata(_minutiChiamata:number): number {
+        return _minutiChiamata
+    }
+    numero404(_importoRicarica:number, _minutiChiamata:number): number {
+        return this.credito + this.ricarica(_importoRicarica) - (this.tariffa * this.chiamata(_minutiChiamata) * this.numeroChiamate)
+    }
+    getNumeroChiamate(): number {
+        return this.numeroChiamate
+    }
+    azzeraChiamate(): number {
+        return this.numeroChiamate = 0
+    }
+}
+
+// prima istanza + console.log
+let primoAbbonato = new nuovoAbbonamento (30, 2, 0.2); //credito, numeroChiamate, tariffa
+console.log('');
+console.log('Primo utente');
+console.log('- - - - - - - - - -');
+console.log(primoAbbonato);
+console.log(`Ricarica: ${primoAbbonato.ricarica(20)} €`); //importoRicarica
+console.log(`Chiamate di ${primoAbbonato.chiamata(15)} minuti`); //minutiChiamata
+console.log(`Credito residuo: ${primoAbbonato.numero404(20, 15)} €`); //importoRicarica, minutiChiamata
+console.log(`Numero chiamate: ${primoAbbonato.getNumeroChiamate()}`);
+console.log(`Chiamate azzerate: ${primoAbbonato.azzeraChiamate()}`);
+
+// prima istanza + console.log
+let secondoAbbonato = new nuovoAbbonamento (30, 2, 0.4); //credito, numeroChiamate, tariffa
+console.log('');
+console.log('Secondo utente');
+console.log('- - - - - - - - - -');
+console.log(secondoAbbonato);
+console.log(`Ricarica: ${secondoAbbonato.ricarica(20)} €`); //importoRicarica
+console.log(`Chiamate di ${secondoAbbonato.chiamata(15)} minuti`); //minutiChiamata
+console.log(`Credito residuo: ${secondoAbbonato.numero404(20, 15)} €`); //importoRicarica, minutiChiamata
+console.log(`Numero chiamate: ${secondoAbbonato.getNumeroChiamate()}`);
+console.log(`Chiamate azzerate: ${secondoAbbonato.azzeraChiamate()}`);
+
+// terza istanza + console.log
+let terzoAbbonato = new nuovoAbbonamento (30, 2, 0.6); //credito, numeroChiamate, tariffa
+console.log('');
+console.log('Terzo utente');
+console.log('- - - - - - - - - -');
+console.log(terzoAbbonato);
+console.log(`Ricarica: ${terzoAbbonato.ricarica(20)} €`); //importoRicarica
+console.log(`Chiamate di ${terzoAbbonato.chiamata(15)} minuti`); //minutiChiamata
+console.log(`Credito residuo: ${terzoAbbonato.numero404(20, 15)} €`); //importoRicarica, minutiChiamata
+console.log(`Numero chiamate: ${terzoAbbonato.getNumeroChiamate()}`);
+console.log(`Chiamate azzerate: ${terzoAbbonato.azzeraChiamate()}`);
+
+
+
+// - - - - - - - - - - - - - - - - - - - - SOLUZIONE CON FETCH + SUPER CLASSE E CLASSE ESTESA - - - - - - - - - - - - - - - 
+console.log('');
+console.log('- - - - - - - - - - - - - - - SOLUZIONE CON FETCH + SUPER CLASSE E CLASSE ESTESA - - - - - - - - - - - - - - - ');
+// definizione super classe
+class Telefonia {
+    credito: number;
+    numeroChiamate: number;
+    constructor (_credito:number, _numeroChiamate:number) {
+        this.credito = _credito;
+        this.numeroChiamate = _numeroChiamate;
+    }
+    ricarica(_importoRicarica:number): number {
+        return _importoRicarica
+    }
+    chiamata(_minutiChiamata:number): number {
+        return _minutiChiamata
+    }
+    getNumeroChiamate(): number {
+        return this.numeroChiamate
+    }
+    azzeraChiamate(): number {
+        return this.numeroChiamate = 0
+    }
+}
+
+// definizione classe estesa
+class telefoniaEstesa extends Telefonia {
+tariffa: number;
+utente: string;
+constructor (_credito:number, _numeroChiamate:number, _tariffa:number, _utente:string) {
+    super (_credito, _numeroChiamate);
+    this.tariffa = _tariffa;
+    this.utente = _utente
+}
+ricarica(_importoRicarica:number): number {
+    return _importoRicarica
+}
+chiamata(_minutiChiamata:number): number {
+    return _minutiChiamata
+}
+numero404(_importoRicarica:number, _minutiChiamata:number): number {
+    return this.credito + this.ricarica(_importoRicarica) - (this.tariffa * this.chiamata(_minutiChiamata) * this.numeroChiamate)
+}
+getNumeroChiamate(): number {
+    return this.numeroChiamate
+}
+azzeraChiamate(): number {
+    return this.numeroChiamate = 0
+}
+}
+
+// fetch
+window.onload = async () => {
+    let promise = await fetch('utenti.json');
+        if (promise.ok) {
+            let utenti = await promise.json();
+            console.log(utenti);
+            for(let i = 0; i < utenti.length; i++) {
+                let nuovaTelefonia = new telefoniaEstesa(utenti[i].credito, utenti[i].numeroChiamate, utenti[i].tariffa, utenti[i].utente);
+                console.log('');
+                console.log(`${nuovaTelefonia.utente}`);
+                console.log('- - - - - - - - - -');
+                console.log(nuovaTelefonia);
+                console.log(`Ricarica: ${nuovaTelefonia.ricarica(20)} €`); //importoRicarica
+                console.log(`Chiamate di ${nuovaTelefonia.chiamata(15)} minuti`); //minutiChiamata
+                console.log(`Credito residuo: ${nuovaTelefonia.numero404(20, 15)} €`); //importoRicarica, minutiChiamata
+                console.log(`Numero chiamate: ${nuovaTelefonia.getNumeroChiamate()}`);
+                console.log(`Chiamate azzerate: ${nuovaTelefonia.azzeraChiamate()}`);
+            }
+        } else {
+            throw new Error('Richiesta non a buon fine')
+        }
+}
