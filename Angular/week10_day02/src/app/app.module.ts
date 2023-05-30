ec827b48331a03fdfc4f979ed07925635b661a56
NunzioDiCroce <nunziodavide@gmail.com>
2023-05-30 15:33:45 +0200

    commit, routing done

--- Angular/week10_day02/src/app/app.module.ts
+++ Angular/week10_day02/src/app/app.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// importo Route e RouterModule per implementare il ROUTING
+import { Route, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PostAttiviComponent } from './post-attivi/post-attivi.component';
 import { PostNonAttiviComponent } from './post-non-attivi/post-non-attivi.component';
+
+// dichiaro array routes per implementare il ROUTING
+const routes: Route[] = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'postAttivi',
+    component: PostAttiviComponent
+  },
+  {
+    path: 'postNonAttivi',
+    component: PostNonAttiviComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
@@ -14,7 +34,9 @@ import { PostNonAttiviComponent } from './post-non-attivi/post-non-attivi.compon
     PostNonAttiviComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // per implementare il ROUTING applico a RouterModule il metodo forRoot con parametro l'array routes dichiarato
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
