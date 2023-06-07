8572e9aae5b18d875643a253fb0d8cd640921b88
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-07 02:50:44 +0200

    commit

--- Angular/week11_day02/src/app/components/reactive/reactive.component.ts
+++ Angular/week11_day02/src/app/components/reactive/reactive.component.ts
@@ -13,16 +13,34 @@ import { FormBuilder, FormControl, FormGroup, Validators, FormArray } from '@ang
 })
 export class ReactiveComponent implements OnInit {
 
-  // dichiarazione FormGroup
+  // definizione FormGroup
   form!: FormGroup;
+
+  // definizione oggetto per gestire i dati ricevuti dal form
+  superHero = {
+    name: '',
+    alterego: '',
+    power: '',
+    enemy: '',
+    planet: '',
+    weakness: ''
+  }
 
   constructor(private builder: FormBuilder) { } // inserimento parametro FromBuilder
 
   ngOnInit(): void {
 
     this.form = this.builder.group({
+      name: this.builder.control(null, [Validators.required]),
+      alterego: this.builder.control(null, [Validators.required]),
+      power: this.builder.control(null, [Validators.required]),
+      enemy: this.builder.control(null, [Validators.required, Validators.maxLength(10)]),
+      planet: this.builder.control(null, [Validators.required, Validators.minLength(5)]),
 
     })
+  }
+
+  createHero() {
   }
 
 }
