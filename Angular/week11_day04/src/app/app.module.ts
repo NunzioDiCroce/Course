1bed688cf451fa55c103290730fbbf489793f998
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-08 15:51:44 +0200

    commit

--- Angular/week11_day04/src/app/app.module.ts
+++ Angular/week11_day04/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// import Route, RouterModule
+import { Route, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -10,6 +13,45 @@ import { ActivePostsComponent } from './components/active-posts/active-posts.com
 import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
 import { PostCardComponent } from './components/post-card/post-card.component';
 import { PostDetailsComponent } from './components/post-details/post-details.component';
+
+// definizione rotte
+const routes: Route[] = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'activePosts',
+    component:ActivePostsComponent
+  },
+  {
+    path:'activePosts/:id',
+    component:PostDetailsComponent
+  },
+  {
+    path:'inactivePosts',
+    component:InactivePostsComponent
+  },
+  {
+    path:'inactivePosts/:id',
+    component:PostDetailsComponent
+  },
+  {
+    path:'users',
+    component:UsersComponent,
+    children:[
+      {
+        path:':id',
+        component:UserDetailsComponent
+      }
+    ]
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+]
+
 
 @NgModule({
   declarations: [
@@ -24,7 +66,9 @@ import { PostDetailsComponent } from './components/post-details/post-details.com
     PostDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // metodo forRoot su RouterModule con parametro (routes)
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
