c00125a6b66cdcb2be15e8deb82d484f540799e3
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-08 15:06:41 +0200

    commit

--- Angular/week11_day04/src/app/app.module.ts
+++ Angular/week11_day04/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { ActivePostsComponent } from './components/active-posts/active-posts.component';
+import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    UsersComponent,
+    UserDetailsComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
+    PostCardComponent,
+    PostDetailsComponent
   ],
   imports: [
     BrowserModule
