130c8379384eb61540a972071cbbab70de303300
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-08 19:30:14 +0200

    commit

--- Angular/week11_day04/src/app/components/user-details/user-details.component.ts
+++ Angular/week11_day04/src/app/components/user-details/user-details.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+// interface import
+import { User } from 'src/app/models/user.interface';
+// service import
+import { UsersService } from 'src/app/services/users.service';
+
+// !!!!!!!!!! ActivatedRoute from @angular/router import !!!!!!!!!!
+import { ActivatedRoute } from '@angular/router';
+
+// !!!!!!!!!! Subscription from @rxjs import !!!!!!!!!!
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -9,9 +19,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  // !!!!!!!!!! definzione variabile per SUBSCRIPTION/SUBSCRIBE !!!!!!!!!!
+  sub!: Subscription;
+
+  // definzione variabile user
+  user!: User | undefined;
+
+  // !!!!!!!!!! inserimento parametri nel constructor. ActivatedRouter !!!!!!!!!!
+  constructor( private userSrv:UsersService, private router:ActivatedRoute ) { }
 
   ngOnInit(): void {
+    // !!!!!!!!!!! definizione metodo per raggiungere i dettagli dello user selezionato in base ad uno dei parametri ovvero lo user.id !!!!!!!!!!!
+    this.sub = this.router.params.subscribe((_parameters) => {
+      const idOfUser = +_parameters['id'];
+      this.user = this.userSrv.recuperaUser(idOfUser)
+    })
+  }
+
+  // !!!!!!!!!! definizione ngOnDestroy per UNSUBSCRIBE !!!!!!!!!!!
+  ngOnDestroy(): void {
+    // !!!!!!!!! definizione metodo di unsubscribe all'OnDestroy !!!!!!!!!!
+    this.sub.unsubscribe()
   }
 
 }
