d9cdcb308e7bdbfdf930d25fdb88fb412ba3312d
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-09 16:06:08 +0200

    commit

--- Angular/ProgettoSettimana11/src/app/app.module.ts
+++ Angular/ProgettoSettimana11/src/app/app.module.ts
@@ -1,20 +1,66 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { Route, RouterModule } from '@angular/router';
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const routes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  /*{
+    path:'login',
+    component:LoginComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path:'register',
+    component:RegisterComponent,
+    canActivate: [AuthGuard]
+  },*/
+  {
+    path:'**',
+    redirectTo:''
+  }
+]
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     MoviesComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    RouterModule.forRoot(routes)
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   ],
   providers: [],
   bootstrap: [AppComponent]
