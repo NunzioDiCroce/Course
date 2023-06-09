b22d0f90e2e79fa6c6c3f35dd19325c3194064dd
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-09 13:00:37 +0200

    commit

--- Angular/week11_day04/src/app/services/users.service.ts
+++ Angular/week11_day04/src/app/services/users.service.ts
@@ -6,6 +6,10 @@ import { User } from '../models/user.interface';
 // environment import
 import { environment } from 'src/environments/environment';
 
+// HttpClient import per recuperare utenti da db.json
+import { HttpClient } from '@angular/common/http';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -33,16 +37,23 @@ export class UsersService {
     }
 ];
 
-  constructor() { }
+  //constructor() { }
 
-  // definizione metodo per recuperare gli uetnti
-  recuperaUsers() {
+  // definizione metodo per recuperare gli utenti
+  /*recuperaUsers() {
     return this.users
-  }
+  }*/
 
   // definizione metodo per recuperare un utente specifico
   recuperaUser(_id:number) {
     return this.users.find((_user) => {_user.id === _id})
   }
 
+
+  // definizione variabili, costruttore, metodi per RECUPERO UTENTI DA DB.JSON
+  baseURL = environment.baseURL;
+  constructor( private http:HttpClient ) {}
+  recuperaUsers() {
+    return this.http.get<User[]>(`${this.baseURL}users`)
+  }
 }
