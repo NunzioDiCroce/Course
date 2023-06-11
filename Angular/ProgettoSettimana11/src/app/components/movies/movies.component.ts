1c063535164010a590509edd1041637b601e6bfd
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-12 01:44:01 +0200

    commit

--- Angular/ProgettoSettimana11/src/app/components/movies/movies.component.ts
+++ Angular/ProgettoSettimana11/src/app/components/movies/movies.component.ts
@@ -22,21 +22,68 @@ export class MoviesComponent implements OnInit {
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   user!: AuthData | null;
   movies: Movie[] = [];
-  favoriteMovies:Favorite[] = [];
+  favoriteMovies: Favorite[] = [];
+  favoriteMovie!: Favorite | null;
+  sub!: Subscription;
+  favoriteSub!: Subscription;
 
   constructor( private moviesSrv:MoviesService, private authSrv:AuthService ) { }
 
   ngOnInit(): void {
 
-    this.authSrv.user$.subscribe((_user) => {this.user = _user});
+    this.authSrv.user$.subscribe((_user) => {
+      this.user = _user
+    });
     console.log(this.user);
 
-    this.moviesSrv.getMovies().subscribe((_movies:Movie[]) => {this.movies = _movies});
+    this.moviesSrv.getMovies().subscribe((_movies:Movie[]) => {
+      this.movies = _movies
+    });
     console.log(this.movies);
 
-    this.moviesSrv.getFavorites(this.user?.user.id!).subscribe((_favoriteMovies:Favorite[]) => {this.favoriteMovies = _favoriteMovies});
+    this.moviesSrv.getFavorites(this.user?.user.id!).subscribe((_favoriteMovies:Favorite[]) => {
+      this.favoriteMovies = _favoriteMovies
+    });
     console.log(this.favoriteMovies)
 
+  }
+
+  likedMovies(_movieId:number) {
+    return this.favoriteMovies.some((_favoriteMovie) => {
+      _favoriteMovie.id === _movieId
+    })
+  }
+
+  iLikeMovie(_movieId:number, _userId:number) {
+    const movieWithLike = this.likedMovies(_movieId);
+    const favoriteMovieId = this.favoriteMovies.find((_favoriteMovie) => {
+      _favoriteMovie.id === _movieId
+    })?.id;
+
+    this.favoriteMovie = {movieId: _movieId, userId: _userId, id: favoriteMovieId!}
+
+    if(movieWithLike) {
+      if(this.favoriteMovie.id !== undefined) {
+        this.moviesSrv.deleteLike(this.favoriteMovie.id).subscribe(() => {
+          this.favoriteMovies = this.favoriteMovies.filter((_favoriteMovie) => {
+            _favoriteMovie.id !== _movieId
+          })
+        })
+      } else {
+        this.moviesSrv.like(this.favoriteMovie).subscribe((_newFavoriteMovie:Favorite) => {
+          this.favoriteMovies.push(_newFavoriteMovie)
+        })
+      }
+    }
+  }
+
+  ngOnDestroy():void {
+    if(this.sub) {
+      this.sub.unsubscribe()
+    }
+    if(this.favoriteSub) {
+      this.favoriteSub.unsubscribe()
+    }
   }
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
