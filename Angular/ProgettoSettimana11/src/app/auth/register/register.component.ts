4a3a009a17937da25b4e232a8934d0361920c255
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-11 10:57:10 +0200

    commit

--- Angular/ProgettoSettimana11/src/app/auth/register/register.component.ts
+++ Angular/ProgettoSettimana11/src/app/auth/register/register.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +15,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  isLoading = false;
+
+  constructor( private authSrv:AuthService, private router:Router ) { }
 
   ngOnInit(): void {
   }
+
+  registra(form:NgForm) {
+    this.isLoading = true;
+    console.log(form.value);
+    try {
+      this.authSrv.signup(form.value).subscribe();
+      this.router.navigate(['/login']);
+      this.isLoading = false
+    } catch(error:any) {
+      console.log(error);
+      if(error.status == 400) {
+        alert('Email already exists');
+        this.router.navigate(['/register'])
+      }
+      this.isLoading = false
+    }
+  }
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 }
