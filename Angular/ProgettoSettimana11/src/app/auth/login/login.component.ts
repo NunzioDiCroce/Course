7336c1073453cc2179670f8706f0df73859cf272
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-11 13:23:57 +0200

    commit

--- Angular/ProgettoSettimana11/src/app/auth/login/login.component.ts
+++ Angular/ProgettoSettimana11/src/app/auth/login/login.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +15,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  isLoading = false;
+
+  constructor( private authSrv:AuthService, private router:Router ) { }
 
   ngOnInit(): void {
   }
+
+  accedi(form:NgForm) {
+    this.isLoading = true;
+    console.log(form.value);
+    try {
+      this.authSrv.login(form.value).subscribe();
+      this.isLoading = false;
+      alert('Login success!')
+    } catch(error:any) {
+      this.isLoading = false;
+      alert('Wrong login!');
+      console.log(error)
+    }
+  }
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 }
