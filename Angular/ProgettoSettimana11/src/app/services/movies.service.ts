dff4fd415768a634da2acdb640010c5ea2567ee1
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-12 10:40:43 +0200

    commit

--- Angular/ProgettoSettimana11/src/app/services/movies.service.ts
+++ Angular/ProgettoSettimana11/src/app/services/movies.service.ts
@@ -18,19 +18,19 @@ export class MoviesService {
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   constructor( private http:HttpClient ) { }
   getMovies() {
-    return this.http.get<Movie[]>('http://localhost:3000/movies-popular')
+    return this.http.get<Movie[]>('http://localhost:4201/movies-popular')
   }
 
   like(favoriteMovie:Favorite) {
-    return this.http.post<Favorite>('http://localhost:3000/favorites',favoriteMovie)
+    return this.http.post<Favorite>('http://localhost:4201/favorites',favoriteMovie)
   }
 
   getFavorites(_id:number) {
-    return this.http.get<Favorite[]>(`http://localhost:3000/favorites?userId=${_id}`)
+    return this.http.get<Favorite[]>(`http://localhost:4201/favorites?userId=${_id}`)
   }
 
   deleteLike(_id:number) {
-    return this.http.delete(`http://localhost:3000/favorites/${_id}`)
+    return this.http.delete(`http://localhost:4201/favorites/${_id}`)
   }
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
