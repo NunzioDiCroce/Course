8d45f17f9a0f2818b823b4ee46343594b445d3f2
NunzioDiCroce <nunziodavide@gmail.com>
2023-06-26 14:58:26 +0200

    first commit after BackEnd start

--- BackEnd/unit01_week01_day01/EpicodeBackendCourse1/src/it/epicode/be/Helloworld.java
+++ BackEnd/unit01_week01_day01/EpicodeBackendCourse1/src/it/epicode/be/Helloworld.java
@@ -4,8 +4,15 @@ public class Helloworld {
 
 public static void main (String[] args)
 		{
+	
+		// - - - - - - - - - - - - - - - - - - - - ESERCIZIO 1
 		// Stampa a console  la frase
 		System.out.println("This is my first Epicode Java Project!");
+		
+		
+		// - - - - - - - - - - - - - - - - - - - - ESERCIZIO 2
+		
+		
 		}
 }
 
